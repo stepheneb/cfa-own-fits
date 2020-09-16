016b4f665e9dcbca8abae1dcb0232d13821db10a
Stephen Bannasch <stephen.bannasch@gmail.com>
2020-09-16 14:37:57 -0400

    press "Enter" to toggle full screen

--- readbin.js
+++ readbin.js
@@ -86,3 +86,19 @@ const forLoopMinMax = (array) => {
 
   return [min, max];
 };
+
+document.addEventListener("keydown", function (e) {
+  if (e.keyCode == 13) {
+    toggleFullScreen();
+  }
+}, false);
+
+function toggleFullScreen() {
+  if (!document.fullscreenElement) {
+    document.documentElement.requestFullscreen();
+  } else {
+    if (document.exitFullscreen) {
+      document.exitFullscreen();
+    }
+  }
+}
