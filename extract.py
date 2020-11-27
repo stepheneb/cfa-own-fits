0ff1c63e94452535587bb8b9476c5f7aa35b5b5e
Stephen Bannasch <stephen.bannasch@gmail.com>
2020-11-27 02:28:54 -0500

    extract rawdata from fits: normalize, handle int16
    FITS files with int16 data are now also transformed into float32
    before writing out into rawdata image files.
    
    The files are normalized to floating point values from 0 to 10.
    
    add more FITS files and generated rawdata images

--- extract.py
+++ extract.py
@@ -7,6 +7,7 @@ import time
 import glob
 
 import numpy as np
+print()
 print("using numpy version: " +  np.version.version)
 
 import matplotlib
@@ -29,10 +30,12 @@ else:
     print("path not found: " + indir)
     quit()
 
+indir = indir.rstrip("/")
+
 head_tail = os.path.split(indir)
 
 dirname = head_tail[1]
-print("dirname: " + dirname)
+print(f"dirname: {dirname}\n")
 
 outdir = "rawdata/" + dirname
 
@@ -55,11 +58,17 @@ def extract_raw_image_data(infile, outdir):
 
     img = fits[0].read()
 
+    normalize_img_datatype = {
+        "float32": lambda img: img,
+        "float64": lambda img: img.astype('float32'),
+        "int16": lambda img: img.astype('float32') / 3276.8
+    }
+
     dtype = str(img.dtype)
     print("img datatype: " + dtype)
-    if dtype != 'float32':
-        print("converting img to float32")
-        img = img.astype('float32')
+    if (dtype != 'float32'):
+        print(f"normalizing image with datatype {dtype} to float32")
+        img = normalize_img_datatype[dtype](img)
 
     y = len(img)
     x = len(img[0])
@@ -67,8 +76,67 @@ def extract_raw_image_data(infile, outdir):
     print("x: " + str(x))
     print("y: " + str(y))
 
-    print ("min: " + str(np.min(img)))
-    print ("max: " + str(np.max(img)))
+    original_min = np.min(img)
+    original_max = np.max(img)
+
+    print (f"min: {str(original_min)}")
+    print (f"max: {str(original_max)}")
+
+    print("\npercentiles:")
+
+    percentile_001 = np.percentile(img, 0.01)
+    percentile_01 = np.percentile(img, 0.1)
+    percentile_1 = np.percentile(img, 1)
+    percentile_5 = np.percentile(img, 5)
+    percentile_10 = np.percentile(img, 10)
+    percentile_50 = np.percentile(img, 50)
+    percentile_90 = np.percentile(img, 90)
+    percentile_95 = np.percentile(img, 95)
+    percentile_99 = np.percentile(img, 99)
+    percentile_999 = np.percentile(img, 99.9)
+
+    print (f"percentile 0.01: {str(percentile_001)}")
+    print (f"percentile 0.1: {str(percentile_01)}")
+    print (f"percentile 1: {str(percentile_1)}")
+    print (f"percentile 5: {str(percentile_5)}")
+    print (f"percentile 10: {str(percentile_10)}")
+    print (f"percentile 50: {str(percentile_50)}")
+    print (f"percentile 90: {str(percentile_90)}")
+    print (f"percentile 95: {str(percentile_95)}")
+    print (f"percentile 99: {str(percentile_99)}")
+    print (f"percentile 99.9: {str(percentile_999)}")
+
+    print("\nclip:")
+    bottom = percentile_001
+    if (original_min >= 0):
+        bottom = original_min
+    top = percentile_999
+
+    img =  np.clip(img, bottom, top)
+
+    clipped_min = np.min(img)
+    clipped_max = np.max(img)
+
+    print (f"min: {str(clipped_min)}")
+    print (f"max: {str(clipped_max)}")
+
+    print("\noffset")
+    img =  img - clipped_min
+
+    offset_min = np.min(img)
+    offset_max = np.max(img)
+
+    print (f"min: {str(offset_min)}")
+    print (f"max: {str(offset_max)}")
+
+    print("\nscale")
+    img =  img * 10 / (offset_max - offset_min)
+
+    scaled_min = np.min(img)
+    scaled_max = np.max(img)
+
+    print (f"min: {str(scaled_min)}")
+    print (f"max: {str(scaled_max)}")
 
     outfile = outdir + '/' + base_filename + '.bin'
 
@@ -90,5 +158,5 @@ def extract_raw_image_data(infile, outdir):
 
 for entry in os.scandir(indir):
     if (entry.path.endswith(".fits") or entry.path.endswith(".FITS")):
-        print(entry.path)
+        # print(entry.path)
         extract_raw_image_data(entry.path, outdir)
