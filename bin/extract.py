7da06e45f02343cba6988943fac00b019965f8a3
Stephen Bannasch <stephen.bannasch@gmail.com>
2020-12-06 22:36:36 -0500

    extract.py: processed images are a bit less bright

--- bin/extract.py
+++ bin/extract.py
@@ -40,20 +40,18 @@ print(f"dirname: {dirname}")
 outdir = "rawdata/" + dirname
 print(f"outdir: {outdir}")
 
+def percentStr(val):
+    return f"{round(val*100, 2)}%"
 
 def human_size(fsize, units=[' bytes',' KB',' MB',' GB',' TB', ' PB', ' EB']):
     return "{:.2f}{}".format(float(fsize), units[0]) if fsize < 1024 else human_size(fsize / 1024, units[1:])
 
 def extract_raw_image_data(infile, outdir):
-    print("\n--------\n")
-
-    print("processing: "+ infile)
-
     original_filename = os.path.split(infile)[1]
-    print("original_filename: " + original_filename)
-
     base_filename = os.path.splitext(original_filename)[0]
-    print("base_filename: " + base_filename)
+
+    print(f"\n-------- {base_filename} --------\n")
+    print("processing: "+ infile)
 
     if not os.path.exists(outdir):
         print("creating output directory for rawdata: " + outdir)
@@ -65,7 +63,7 @@ def extract_raw_image_data(infile, outdir):
 
     img = fits[0].read()
     img_flat = img.flatten()
-    size = len(img_flat)
+    image_size = len(img_flat)
 
     normalize_img_datatype = {
         "float32": lambda img: img,
@@ -80,7 +78,7 @@ def extract_raw_image_data(infile, outdir):
         print(f"normalizing image with datatype {dtype} to float32")
         img = normalize_img_datatype[dtype](img)
 
-    print(f"size: {size} ({human_size(size)})")
+    print(f"image size: {image_size} ({human_size(image_size)})")
 
     print()
 
@@ -104,21 +102,25 @@ def extract_raw_image_data(infile, outdir):
     percentile_90 = np.percentile(img, 90)
     percentile_95 = np.percentile(img, 95)
     percentile_99 = np.percentile(img, 99)
+    percentile_995 = np.percentile(img, 99.5)
     percentile_999 = np.percentile(img, 99.9)
     percentile_9999 = np.percentile(img, 99.99)
 
-    print (f"percentile 0.001: {str(round(percentile_0001 ,2))}")
-    print (f"percentile 0.01: {str(round(percentile_001 ,2))}")
-    print (f"percentile 0.1: {str(round(percentile_01 ,2))}")
-    print (f"percentile 1: {str(round(percentile_1 ,2))}")
-    print (f"percentile 5: {str(round(percentile_5 ,2))}")
-    print (f"percentile 10: {str(round(percentile_10 ,2))}")
-    print (f"percentile 50: {str(round(percentile_50 ,2))}")
-    print (f"percentile 90: {str(round(percentile_90 ,2))}")
-    print (f"percentile 95: {str(round(percentile_95 ,2))}")
-    print (f"percentile 99: {str(round(percentile_99 ,2))}")
-    print (f"percentile 99.9: {str(round(percentile_999 ,2))}")
-    print (f"percentile 99.99: {str(round(percentile_9999 ,2))}")
+    print (f"percentile 0.001: {str(round(percentile_0001 ,3))}")
+    print (f"percentile 0.01: {str(round(percentile_001 ,3))}")
+    print (f"percentile 0.1: {str(round(percentile_01 ,3))}")
+    print()
+    print (f"percentile 1: {str(round(percentile_1 ,3))}")
+    print (f"percentile 5: {str(round(percentile_5 ,3))}")
+    print (f"percentile 10: {str(round(percentile_10 ,3))}")
+    print (f"percentile 50: {str(round(percentile_50 ,3))}")
+    print (f"percentile 90: {str(round(percentile_90 ,3))}")
+    print (f"percentile 95: {str(round(percentile_95 ,3))}")
+    print (f"percentile 99: {str(round(percentile_99 ,3))}")
+    print()
+    print (f"percentile 99.5: {str(round(percentile_995 ,3))}")
+    print (f"percentile 99.9: {str(round(percentile_999 ,3))}")
+    print (f"percentile 99.99: {str(round(percentile_9999 ,3))}")
 
     print()
 
@@ -132,38 +134,53 @@ def extract_raw_image_data(infile, outdir):
     min_greater_than_zero = img_flat[np.argmax(img_flat > 0)]
 
     count_less_than_zero = np.count_nonzero(img_flat < 0)
+    percent_count_less_than_zero = count_less_than_zero / image_size
+
     count_less_than_next_min = np.count_nonzero(img_flat < next_min)
+    percent_less_than_next_min = count_less_than_next_min / image_size
+
     count_less_than_min_greater_than_next_min = np.count_nonzero(img_flat < min_greater_than_next_min)
+    percent_less_than_min_greater_than_next_min = count_less_than_min_greater_than_next_min / image_size
 
     print (f"min: {str(round(original_min, 3))}")
     print (f"max: {str(round(original_max, 1))}")
 
     print()
 
-    print (f"next min: {str(round(next_min, 3))}")
-    print (f"count: less than next min: {str(count_less_than_next_min)}")
+    print (f"count less than zero: {str(count_less_than_zero)}, {percentStr(percent_count_less_than_zero)}")
 
-    print (f"next after next min: {str(round(min_greater_than_next_min, 3))}")
-    print (f"count: less than next after next min: {str(count_less_than_min_greater_than_next_min)}")
+    print (f"next min: {str(round(next_min, 3))}, count: {str(count_less_than_next_min)}, {percentStr(percent_less_than_next_min)}")
 
-    if next_min <= 0:
-        print()
-        print (f"min greater than zero: {str(round(min_greater_than_zero, 3))}")
-        print (f"count: less than zero: {str(count_less_than_zero)}")
+    print (f"next after next min: {str(round(min_greater_than_next_min, 3))}, count: {str(count_less_than_min_greater_than_next_min)}, {percentStr(percent_less_than_min_greater_than_next_min)}")
 
     print()
 
-    top = percentile_99
-    # if (count_less_than_next_min / size) > 0.1:
-    #     bottom = min
-    if next_min > 0:
-        bottom = next_min
-        print(f"Clipping to next_min and percentile 99: {str(round(bottom, 3))}, {str(round(top, 3))}")
-        img =  np.clip(img, bottom, top)
-    else:
+    count_more_than_99 = np.count_nonzero(img_flat > percentile_99)
+    percent_count_more_than_99 = count_more_than_99 / image_size
+    count_more_than_995 = np.count_nonzero(img_flat > percentile_995)
+    percent_count_more_than_995 = count_more_than_995 / image_size
+    count_more_than_999 = np.count_nonzero(img_flat > percentile_999)
+    percent_count_more_than_999 = count_more_than_999 / image_size
+
+    print (f"count more than {round(percentile_99, 2)} (99 percentile): {str(count_more_than_99)}")
+    print (f"count more than {round(percentile_995, 2)} (99.5 percentile): {str(count_more_than_995)}")
+    print (f"count more than {round(percentile_999, 2)} (99.9 percentile): {str(count_more_than_999)}")
+
+    print()
+
+    top = percentile_999
+    top = percentile_999 - (percentile_999 - percentile_995) / 2
+    # top = percentile_99
+
+    if (count_less_than_next_min / image_size < 0.01) and (count_less_than_min_greater_than_next_min / image_size < 0.01):
+        bottom = percentile_1
+    elif (count_less_than_min_greater_than_next_min / image_size < 0.01):
         bottom = min_greater_than_next_min
-        print(f"Clipping to `next after next min` and percentile 99.9: {str(round(bottom, 3))}, {str(round(top, 3))}")
-        img =  np.clip(img, bottom, top)
+    else:
+        bottom = next_min
+
+    print(f"Clipping to: {str(round(bottom, 3))}, {str(round(top, 3))}")
+    img =  np.clip(img, bottom, top)
 
     clipped_min = np.min(img)
     clipped_max = np.max(img)
@@ -202,24 +219,27 @@ def extract_raw_image_data(infile, outdir):
     percentile_90 = np.percentile(img, 90)
     percentile_95 = np.percentile(img, 95)
     percentile_99 = np.percentile(img, 99)
+    percentile_995 = np.percentile(img, 99.5)
     percentile_999 = np.percentile(img, 99.9)
     percentile_9999 = np.percentile(img, 99.99)
 
-    print (f"percentile 0.001: {str(round(percentile_0001 ,2))}")
-    print (f"percentile 0.01: {str(round(percentile_001 ,2))}")
-    print (f"percentile 0.1: {str(round(percentile_01 ,2))}")
-    print (f"percentile 1: {str(round(percentile_1 ,2))}")
-    print (f"percentile 5: {str(round(percentile_5 ,2))}")
-    print (f"percentile 10: {str(round(percentile_10 ,2))}")
-    print (f"percentile 50: {str(round(percentile_50 ,2))}")
-    print (f"percentile 90: {str(round(percentile_90 ,2))}")
-    print (f"percentile 95: {str(round(percentile_95 ,2))}")
-    print (f"percentile 99: {str(round(percentile_99 ,2))}")
-    print (f"percentile 99.9: {str(round(percentile_999 ,2))}")
-    print (f"percentile 99.99: {str(round(percentile_9999 ,2))}")
+    print (f"percentile 0.001: {str(round(percentile_0001 ,3))}")
+    print (f"percentile 0.01: {str(round(percentile_001 ,3))}")
+    print (f"percentile 0.1: {str(round(percentile_01 ,3))}")
+    print()
+    print (f"percentile 1: {str(round(percentile_1 ,3))}")
+    print (f"percentile 5: {str(round(percentile_5 ,3))}")
+    print (f"percentile 10: {str(round(percentile_10 ,3))}")
+    print (f"percentile 50: {str(round(percentile_50 ,3))}")
+    print (f"percentile 90: {str(round(percentile_90 ,3))}")
+    print (f"percentile 95: {str(round(percentile_95 ,3))}")
+    print (f"percentile 99: {str(round(percentile_99 ,3))}")
+    print()
+    print (f"percentile 99.5: {str(round(percentile_995 ,3))}")
+    print (f"percentile 99.9: {str(round(percentile_999 ,3))}")
+    print (f"percentile 99.99: {str(round(percentile_9999 ,3))}")
 
     print()
-
 
     outfile = outdir + '/' + base_filename + '.bin'
 
