ccb964eda20b97d0114196c47a949f985c04d3bb
Stephen Bannasch <stephen.bannasch@gmail.com>
2020-12-08 17:06:26 -0500

    extract.py: generate full 0.5x 0.25x rawdata images
    example:
    
    rawdata/HST_Lagoon/
    |-- 1300x1250
    |   |-- HST_Lagoon_f502Blue.bin
    |   |-- HST_Lagoon_f656Green.bin
    |   `-- HST_Lagoon_f658Red.bin
    |-- 2600x2500
    |   |-- HST_Lagoon_f502Blue.bin
    |   |-- HST_Lagoon_f656Green.bin
    |   `-- HST_Lagoon_f658Red.bin
    `-- 650x625
        |-- HST_Lagoon_f502Blue.bin
        |-- HST_Lagoon_f656Green.bin
        `-- HST_Lagoon_f658Red.bin

--- bin/extract.py
+++ bin/extract.py
@@ -1,5 +1,10 @@
 #!/usr/local/bin/python3
 
+from fitsio import FITS, FITSHDR
+import fitsio
+import matplotlib.pyplot as plt
+import matplotlib
+from scipy import interpolate
 import sys
 import os.path
 from os import path
@@ -8,14 +13,11 @@ import glob
 
 import numpy as np
 print()
-print("using numpy version: " +  np.version.version)
+print("using numpy version: " + np.version.version)
 
-import matplotlib
-import matplotlib.pyplot as plt
-print("using matplotlib version: " +  matplotlib.__version__)
 
-import fitsio
-from fitsio import FITS,FITSHDR
+print("using matplotlib version: " + matplotlib.__version__)
+
 
 if len(sys.argv) < 2:
     print("*** error: no FITS input file directory specified")
@@ -40,30 +42,87 @@ print(f"dirname: {dirname}")
 outdir = "rawdata/" + dirname
 print(f"outdir: {outdir}")
 
+
 def percentStr(val):
     return f"{round(val*100, 2)}%"
 
-def human_size(fsize, units=[' bytes',' KB',' MB',' GB',' TB', ' PB', ' EB']):
+
+def human_size(fsize, units=[' bytes', ' KB', ' MB', ' GB', ' TB', ' PB', ' EB']):
     return "{:.2f}{}".format(float(fsize), units[0]) if fsize < 1024 else human_size(fsize / 1024, units[1:])
+
+
+def image_file_size(image):
+    return image.size * image.itemsize
+
+
+def scale_image(scale, image):
+    [ny, nx] = image.shape[:2]
+    [n2y, n2x] = np.multiply([ny, nx], scale).astype(int)
+    def xrange(x): return np.linspace(0, 1, x)
+    f = interpolate.interp2d(xrange(nx), xrange(ny), image, kind="linear")
+    scaled_image = f(xrange(n2x), xrange(n2y))
+    return scaled_image
+
+
+def scale_image2(scale, image):
+    [ny, nx] = image.shape[:2]
+    [n2y, n2x] = np.multiply([ny, nx], scale).astype(int)
+    def xrange(x): return np.linspace(0, 1, x)
+    f = interpolate.interp2d(xrange(nx), xrange(ny), image, kind="linear")
+    scaled_image = np.float32(f(xrange(n2x), xrange(n2y)))
+    expected_size = n2x * n2y * 4
+    actual_size = image_file_size(scaled_image)
+    dt = str(scaled_image.dtype)
+    return scaled_image
+
+
+def write_img_to_file(image, base_filename, outdir, scale=1):
+    if scale != 1:
+        image = scale_image2(scale, image)
+
+    [ny, nx] = image.shape[:2]
+    outpath = f"{outdir}/{nx}x{ny}"
+
+    if not os.path.exists(outpath):
+        print("creating output directory for rawdata: " + outpath)
+        os.makedirs(outpath)
+
+    outfile = f"{outpath}/{base_filename}.bin"
+
+    output_file = open(outfile, 'wb')
+    image.tofile(output_file)
+    output_file.close()
+
+    actual_size = image_file_size(image)
+    if scale == 1:
+        print("writing rawdata image: " + outfile)
+
+    else:
+        print(f"writing {scale}x rawdata image: " + outpath)
+
+    print(f"file size: {actual_size},  ({human_size(actual_size)}")
+    print()
+
 
 def extract_raw_image_data(infile, outdir):
     original_filename = os.path.split(infile)[1]
     base_filename = os.path.splitext(original_filename)[0]
 
     print(f"\n-------- {base_filename} --------\n")
-    print("processing: "+ infile)
+    print("processing: " + infile)
 
     if not os.path.exists(outdir):
         print("creating output directory for rawdata: " + outdir)
         os.makedirs(outdir)
 
-    fits=fitsio.FITS(infile)
+    fits = fitsio.FITS(infile)
 
     print("hdus: " + str(len(fits)))
 
     img = fits[0].read()
     img_flat = img.flatten()
-    image_size = len(img_flat)
+    image_size = img.size
+    file_size = image_file_size(img)
 
     normalize_img_datatype = {
         "float32": lambda img: img,
@@ -78,12 +137,11 @@ def extract_raw_image_data(infile, outdir):
         print(f"normalizing image with datatype {dtype} to float32")
         img = normalize_img_datatype[dtype](img)
 
-    print(f"image size: {image_size} ({human_size(image_size)})")
+    print(f"image size: {file_size} ({human_size(file_size)})")
 
     print()
 
-    y = len(img)
-    x = len(img[0])
+    [x, y] = img.shape[:2]
 
     print("x: " + str(x))
     print("y: " + str(y))
@@ -106,21 +164,21 @@ def extract_raw_image_data(infile, outdir):
     percentile_999 = np.percentile(img, 99.9)
     percentile_9999 = np.percentile(img, 99.99)
 
-    print (f"percentile 0.001: {str(round(percentile_0001 ,3))}")
-    print (f"percentile 0.01: {str(round(percentile_001 ,3))}")
-    print (f"percentile 0.1: {str(round(percentile_01 ,3))}")
+    print(f"percentile 0.001: {str(round(percentile_0001 ,3))}")
+    print(f"percentile 0.01: {str(round(percentile_001 ,3))}")
+    print(f"percentile 0.1: {str(round(percentile_01 ,3))}")
     print()
-    print (f"percentile 1: {str(round(percentile_1 ,3))}")
-    print (f"percentile 5: {str(round(percentile_5 ,3))}")
-    print (f"percentile 10: {str(round(percentile_10 ,3))}")
-    print (f"percentile 50: {str(round(percentile_50 ,3))}")
-    print (f"percentile 90: {str(round(percentile_90 ,3))}")
-    print (f"percentile 95: {str(round(percentile_95 ,3))}")
-    print (f"percentile 99: {str(round(percentile_99 ,3))}")
+    print(f"percentile 1: {str(round(percentile_1 ,3))}")
+    print(f"percentile 5: {str(round(percentile_5 ,3))}")
+    print(f"percentile 10: {str(round(percentile_10 ,3))}")
+    print(f"percentile 50: {str(round(percentile_50 ,3))}")
+    print(f"percentile 90: {str(round(percentile_90 ,3))}")
+    print(f"percentile 95: {str(round(percentile_95 ,3))}")
+    print(f"percentile 99: {str(round(percentile_99 ,3))}")
     print()
-    print (f"percentile 99.5: {str(round(percentile_995 ,3))}")
-    print (f"percentile 99.9: {str(round(percentile_999 ,3))}")
-    print (f"percentile 99.99: {str(round(percentile_9999 ,3))}")
+    print(f"percentile 99.5: {str(round(percentile_995 ,3))}")
+    print(f"percentile 99.9: {str(round(percentile_999 ,3))}")
+    print(f"percentile 99.99: {str(round(percentile_9999 ,3))}")
 
     print()
 
@@ -139,19 +197,20 @@ def extract_raw_image_data(infile, outdir):
     count_less_than_next_min = np.count_nonzero(img_flat < next_min)
     percent_less_than_next_min = count_less_than_next_min / image_size
 
-    count_less_than_min_greater_than_next_min = np.count_nonzero(img_flat < min_greater_than_next_min)
+    count_less_than_min_greater_than_next_min = np.count_nonzero(
+        img_flat < min_greater_than_next_min)
     percent_less_than_min_greater_than_next_min = count_less_than_min_greater_than_next_min / image_size
 
-    print (f"min: {str(round(original_min, 3))}")
-    print (f"max: {str(round(original_max, 1))}")
+    print(f"min: {str(round(original_min, 3))}")
+    print(f"max: {str(round(original_max, 1))}")
 
     print()
 
-    print (f"count less than zero: {str(count_less_than_zero)}, {percentStr(percent_count_less_than_zero)}")
+    print(f"count less than zero: {str(count_less_than_zero)}, {percentStr(percent_count_less_than_zero)}")
 
-    print (f"next min: {str(round(next_min, 3))}, count: {str(count_less_than_next_min)}, {percentStr(percent_less_than_next_min)}")
+    print(f"next min: {str(round(next_min, 3))}, count: {str(count_less_than_next_min)}, {percentStr(percent_less_than_next_min)}")
 
-    print (f"next after next min: {str(round(min_greater_than_next_min, 3))}, count: {str(count_less_than_min_greater_than_next_min)}, {percentStr(percent_less_than_min_greater_than_next_min)}")
+    print(f"next after next min: {str(round(min_greater_than_next_min, 3))}, count: {str(count_less_than_min_greater_than_next_min)}, {percentStr(percent_less_than_min_greater_than_next_min)}")
 
     print()
 
@@ -162,9 +221,9 @@ def extract_raw_image_data(infile, outdir):
     count_more_than_999 = np.count_nonzero(img_flat > percentile_999)
     percent_count_more_than_999 = count_more_than_999 / image_size
 
-    print (f"count more than {round(percentile_99, 2)} (99 percentile): {str(count_more_than_99)}")
-    print (f"count more than {round(percentile_995, 2)} (99.5 percentile): {str(count_more_than_995)}")
-    print (f"count more than {round(percentile_999, 2)} (99.9 percentile): {str(count_more_than_999)}")
+    print(f"count more than {round(percentile_99, 2)} (99 percentile): {str(count_more_than_99)}")
+    print(f"count more than {round(percentile_995, 2)} (99.5 percentile): {str(count_more_than_995)}")
+    print(f"count more than {round(percentile_999, 2)} (99.9 percentile): {str(count_more_than_999)}")
 
     print()
 
@@ -180,30 +239,30 @@ def extract_raw_image_data(infile, outdir):
         bottom = next_min
 
     print(f"Clipping to: {str(round(bottom, 3))}, {str(round(top, 3))}")
-    img =  np.clip(img, bottom, top)
+    img = np.clip(img, bottom, top)
 
     clipped_min = np.min(img)
     clipped_max = np.max(img)
-    print (f"clipped min: {str(round(clipped_min, 3))}")
-    print (f"clipped max: {str(round(clipped_max, 3))}")
+    print(f"clipped min: {str(round(clipped_min, 3))}")
+    print(f"clipped max: {str(round(clipped_max, 3))}")
 
     print("\nShifting data to 0")
-    img =  img - clipped_min
+    img = img - clipped_min
 
     offset_min = np.min(img)
     offset_max = np.max(img)
 
-    print (f"shifted min: {str(round(offset_min, 3))}")
-    print (f"shifted max: {str(round(offset_max, 3))}")
+    print(f"shifted min: {str(round(offset_min, 3))}")
+    print(f"shifted max: {str(round(offset_max, 3))}")
 
     print("\nRescaling data to 0..10")
-    img =  img * 10 / (offset_max - offset_min)
+    img = img * 10 / (offset_max - offset_min)
 
     scaled_min = np.min(img)
     scaled_max = np.max(img)
 
-    print (f"rescaled min: {str(round(scaled_min, 3))}")
-    print (f"rescaled max: {str(round(scaled_max, 3))}")
+    print(f"rescaled min: {str(round(scaled_min, 3))}")
+    print(f"rescaled max: {str(round(scaled_max, 3))}")
 
     print()
 
@@ -223,31 +282,27 @@ def extract_raw_image_data(infile, outdir):
     percentile_999 = np.percentile(img, 99.9)
     percentile_9999 = np.percentile(img, 99.99)
 
-    print (f"percentile 0.001: {str(round(percentile_0001 ,3))}")
-    print (f"percentile 0.01: {str(round(percentile_001 ,3))}")
-    print (f"percentile 0.1: {str(round(percentile_01 ,3))}")
+    print(f"percentile 0.001: {str(round(percentile_0001 ,3))}")
+    print(f"percentile 0.01: {str(round(percentile_001 ,3))}")
+    print(f"percentile 0.1: {str(round(percentile_01 ,3))}")
     print()
-    print (f"percentile 1: {str(round(percentile_1 ,3))}")
-    print (f"percentile 5: {str(round(percentile_5 ,3))}")
-    print (f"percentile 10: {str(round(percentile_10 ,3))}")
-    print (f"percentile 50: {str(round(percentile_50 ,3))}")
-    print (f"percentile 90: {str(round(percentile_90 ,3))}")
-    print (f"percentile 95: {str(round(percentile_95 ,3))}")
-    print (f"percentile 99: {str(round(percentile_99 ,3))}")
+    print(f"percentile 1: {str(round(percentile_1 ,3))}")
+    print(f"percentile 5: {str(round(percentile_5 ,3))}")
+    print(f"percentile 10: {str(round(percentile_10 ,3))}")
+    print(f"percentile 50: {str(round(percentile_50 ,3))}")
+    print(f"percentile 90: {str(round(percentile_90 ,3))}")
+    print(f"percentile 95: {str(round(percentile_95 ,3))}")
+    print(f"percentile 99: {str(round(percentile_99 ,3))}")
     print()
-    print (f"percentile 99.5: {str(round(percentile_995 ,3))}")
-    print (f"percentile 99.9: {str(round(percentile_999 ,3))}")
-    print (f"percentile 99.99: {str(round(percentile_9999 ,3))}")
+    print(f"percentile 99.5: {str(round(percentile_995 ,3))}")
+    print(f"percentile 99.9: {str(round(percentile_999 ,3))}")
+    print(f"percentile 99.99: {str(round(percentile_9999 ,3))}")
 
     print()
 
-    outfile = outdir + '/' + base_filename + '.bin'
-
-    output_file = open(outfile, 'wb')
-    img.tofile(output_file)
-    output_file.close()
-
-    print("writing: " + outfile)
+    write_img_to_file(img, base_filename, outdir)
+    write_img_to_file(img, base_filename, outdir, 0.5)
+    write_img_to_file(img, base_filename, outdir, 0.25)
 
     # fig, ax = plt.subplots(2)
     # ax[0].hist(img.flatten(), bins=100, range=(0.1, 40), density=False);
