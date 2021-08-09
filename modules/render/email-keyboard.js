9ab3b76c18482bd8e15668a189e73d95b956c0fe
Stephen Bannasch <stephen.bannasch@gmail.com>
2021-08-09 01:44:20 -0400

    emailKeyboard: remove more keys
    removed from lower-case:
    
    ` =
    [ ] \
    
    removed from upper-case:
    
    ~
    { } |
    < > ?

--- modules/render/email-keyboard.js
+++ modules/render/email-keyboard.js
@@ -33,17 +33,17 @@ emailKeyboard.render = (page, registeredCallbacks) => {
     keyboard.setOptions({
       layout: {
         'default': [
-          '` 1 2 3 4 5 6 7 8 9 0 - = {bksp}',
-          'q w e r t y u i o p [ ] \\',
-          'a s d f g h j k l ; \'',
-          '{shift} z x c v b n m , . / {shift}',
+          '1 2 3 4 5 6 7 8 9 0 - {bksp}',
+          'q w e r t y u i o p',
+          'a s d f g h j k l',
+          '{shift} z x c v b n m . {shift}',
           '.com @'
         ],
         'shift': [
-          '~ ! @ # $ % ^ & * _ + {bksp}',
-          'Q W E R T Y U I O P { } |',
-          'A S D F G H J K L : "',
-          '{shift} Z X C V B N M < > ? {shift}',
+          '` ~ ! @ # $ % ^ & * _ + {bksp}',
+          'Q W E R T Y U I O P',
+          'A S D F G H J K L :',
+          '{shift} Z X C V B N M {shift}',
           '.com @'
         ]
       }
