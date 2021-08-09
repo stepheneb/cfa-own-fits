cb130d87f6995516aa4d612e1727c217d690a16d
Stephen Bannasch <stephen.bannasch@gmail.com>
2021-08-08 22:19:58 -0400

    emailKeyboard: remove CAPS lock

--- modules/render/email-keyboard.js
+++ modules/render/email-keyboard.js
@@ -34,15 +34,15 @@ emailKeyboard.render = (page, registeredCallbacks) => {
       layout: {
         'default': [
           '` 1 2 3 4 5 6 7 8 9 0 - = {bksp}',
-          ' q w e r t y u i o p [ ] \\',
-          '{lock} a s d f g h j k l ; \'',
+          'q w e r t y u i o p [ ] \\',
+          'a s d f g h j k l ; \'',
           '{shift} z x c v b n m , . / {shift}',
           '.com @'
         ],
         'shift': [
           '~ ! @ # $ % ^ & * _ + {bksp}',
-          ' Q W E R T Y U I O P { } |',
-          '{lock} A S D F G H J K L : "',
+          'Q W E R T Y U I O P { } |',
+          'A S D F G H J K L : "',
           '{shift} Z X C V B N M < > ? {shift}',
           '.com @'
         ]
